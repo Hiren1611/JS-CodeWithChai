f4a7e9a2c7822a29acce867499515f1020de01ed
Hiren Prajapati <99706696+Hiren1611@users.noreply.github.com>
2024-09-11 10:24:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";  // treat all JS code as newer version
+
+// alert( 3 + 3)  // we are using nodejs, not browser
+
+console.log( 3
+    +3) // code readability  should be high
+
+console.log("Hiren");
+
+
+let name = "Hiren"
+let age = 18
+let  isLoggedIn = false
+let state;
+
+
+// number => 2  to power 53
+// bigint 
+// string => ""
+// Boolean => true/false
+// null => standalone value (Empty)
+// undefined => value not assigned yet
+// symbol => unique
+
+//object
+
+console.log(typeof "Hiren");
+console.log(typeof age); 
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
