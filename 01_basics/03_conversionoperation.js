84427962c7b71db6683e7395260e07b4dab2c18e
Hiren Prajapati <99706696+Hiren1611@users.noreply.github.com>
2024-09-16 08:44:31 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,41 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null 
+// let score = undefined
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score)); 2nd way for check datatype of given number
+
+// let valueInNumber =  Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+//  33abc => NAN
+//  null => 0
+// undefined => NAN
+// true => 1 ; false => 0 ;
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+// let isLoggedIn = "Hiren"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+
+
+// 1 => true ; 0 => false
+// "" => false 
+// "Hiren" => True
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
