f22273a5d1ae2fa9e9170a41c9daa11c4f3ced26
Hiren Prajapati <99706696+Hiren1611@users.noreply.github.com>
2024-09-16 09:21:04 +0000

    operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -37,5 +37,68 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//******************  Operations  ************************************************************** */
+
+
+let value = 6
+let negvalue = -value
+
+// console.log(negvalue);
+
+
+// basic Maths Operation
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**3); // 2's power 3
+// console.log(2%3);  // reminder
+
+
+let str1 = "Hiren "
+let str2 = "Prajapati"
+
+let str3 = str1+ str2;
+// console.log(str3);
+
+// console.log("1" + 2); //12 no conversion for string to number
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2 ); //122
+// console.log(1+ 2 + "2" ); //32  // don't write code like this
+
+
+// console.log( (3 + 4) * 5 % 3) 
+// always use bracket and don't write code those who are not easy to understand
+
+// console.log(true+)  // not applicable
+// console.log(+true)  //use for conversion don't use this kind of code 
+// console.log(+"")  
+
+// let num1 , num2 ,num3 
+// num1 = num2 = num3 = 2+2 
+// // don't declare  variable like this 
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+//  prefix 
+
+let x = 4;
+let y = ++x;
+
+console.log([x],[y]); // op: x = 5 , y = 5
+
+
+// postfix
+let a = 4;
+let b = a++;
+
+console.log([a],[b]); // op: a = 5 , b = 4
+
+
